11514216444c4854df50cb9cbd358527633dfcac
LRS4 <lrspencer@hotmail.co.uk>
2020-06-14 21:05:18 +0100

    Initial commit

---
+++ Advantage.API/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advantage.API.Models
+{
+    public class Order
+    {
+        public int Id;
+        public Customer Customer { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Placed { get; set; }
+        public DateTime? Completed { get; set; }
+
+    }
+}
