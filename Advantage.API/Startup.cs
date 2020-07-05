68887b358d2178eb3434b4085191a00cdce32d2f
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-07-05 18:55:23 +0100

    Add postgres connection string user secret to Startup.cs

--- Advantage.API/Startup.cs
+++ Advantage.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Advantage.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,6 +17,8 @@ namespace Advantage.API
 {
     public class Startup
     {
+        private string _connectionString = null;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,7 +29,14 @@ namespace Advantage.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            _connectionString = Configuration["secretConnectionString"];
+
             services.AddControllers();
+
+            services.AddMvc();
+
+            services.AddDbContext<ApiContext>(
+                options => options.UseNpgsql(_connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
